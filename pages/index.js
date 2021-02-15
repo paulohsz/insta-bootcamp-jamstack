96d3a7240aabd5b93592b0c3ae9a133ac639d307
Paulo Henrique Stocco Zancanaro <paulostoc@gmail.com>
2021-02-15 19:36:45 +0000

    exchanged div for box

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Menu from '../src/components/commons/Menu'
 import { Text } from '../src/components/foundation/Text';
 import { Button } from '../src/components/commons/Button';
 import { Grid } from '../src/components/layout/Grid';
+import { Box } from '../src/components/layout/Box';
 
 const Title = styled.h1`
   font-size: 50px;
@@ -12,20 +13,27 @@ const Title = styled.h1`
 
 export default function Home() {
   return (
-    <div style={{
-      flex: '1',
-      display: 'flex',
-      flexWrap: 'wrap',
-      flexDirection: 'column',
-      justifyContent: 'space-between',
-    }}>
+    <Box 
+      flex={1}
+      display="flex"
+      flexWrap="wrap"
+      flexDirection="column"
+      justifyContent="space-between"
+      backgroundImage="url(/images/bubbles.svg)"
+      backgroundRepeat="no-repeat"
+      backgroundPosition="bottom right"
+    >
       <Menu />
 
-      <Grid.Container>
+      <Grid.Container marginTop={{xs: '32px', md: '75px'}}>
         <Grid.Row>
           <Grid.Col
             offset={{ xs: 0, md: 1 }}
             value={{ xs: 12, md: 5 }}
+            display="flex"
+            alignItems="flex-start"
+            justifyContent="center"
+            flexDirection="column"
           >
             <div>
               <Text
@@ -75,6 +83,6 @@ export default function Home() {
       </Grid.Container>
 
       <Footer />
-    </div>
+    </Box>
   );
 }
