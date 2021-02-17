f8bf458f061f26ddc70b431a41cbf8c5ee8af974
Paulo Henrique Stocco Zancanaro <paulostoc@gmail.com>
2021-02-17 23:07:43 +0000

    add ThemeColors

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,20 @@
 import React, { useState } from 'react';
 import { ThemeProvider } from 'styled-components';
 import Head from 'next/head';
-import theme from '../src/theme';
+import theme, { colorsType } from '../src/theme';
 import GlobalStyle from '../src/theme/GlobalStyle';
-
 import ThemeContext from '../src/contexts/theme';
 
 // eslint-disable-next-line react/prop-types
 export default function App({ Component, pageProps }) {
-  const [dark, setDarkTheme] = useState(false);
-  function handleChangeTheme() {
-    setDarkTheme(!dark);
+  const [colors, setThemeColors] = useState({ mode: 'light', ...colorsType.light });
+
+  function toggleDarkMode() {
+    setThemeColors(
+      colors.mode === 'dark'
+        ? { mode: 'light', ...colorsType.light }
+        : { mode: 'dark', ...colorsType.dark },
+    );
   }
 
   return (
@@ -20,8 +24,8 @@ export default function App({ Component, pageProps }) {
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
       </Head>
-      <ThemeContext.Provider value={{ dark, handleChangeTheme }}>
-        <ThemeProvider theme={theme}>
+      <ThemeContext.Provider value={{ colors, toggleDarkMode }}>
+        <ThemeProvider theme={{ ...theme, colors }}>
           <GlobalStyle />
           {/* eslint-disable-next-line react/jsx-props-no-spreading */}
           <Component {...pageProps} />
--- src/contexts/theme.js
+++ src/contexts/theme.js
@@ -1,5 +1,8 @@
 import { createContext } from 'react';
 
-const ThemeContext = createContext();
+const ThemeContext = createContext({
+  dark: true,
+  toggleDarkMode: () => {},
+});
 
 export default ThemeContext;
