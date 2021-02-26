b90bb14e78e876f0069d5959889a588c2b6d1c2b
Paulo Henrique Stocco Zancanaro <paulostoc@gmail.com>
2021-02-26 22:42:49 +0000

    Create textField

--- src/components/commons/Button/index.js
+++ src/components/commons/Button/index.js
@@ -43,6 +43,14 @@ const Button = styled.button`
     `,
   })}
 
+  &:disabled {
+    cursor: not-allowed;
+    opacity: .2;
+  }
+  ${({ fullWidth }) => fullWidth && css`
+    width: 100%;
+  `};
+
 `;
 
 export default Button;
--- src/components/foundation/Text/index.js
+++ src/components/foundation/Text/index.js
@@ -22,10 +22,7 @@ const smallestException = css`
   `}
 `;
 
-export const TextStyleVariants = {
-  smallestException,
-  paragraph1,
-  title: css`
+const title = css`
     ${({ theme }) => css`
       font-size: ${theme.typographyVariants.titleXS.fontSize};
       font-weight: ${theme.typographyVariants.titleXS.fontWeight};
@@ -40,7 +37,12 @@ export const TextStyleVariants = {
         `}
       `,
   })}
-  `,
+`;
+
+export const TextStyleVariants = {
+  smallestException,
+  paragraph1,
+  title,
 };
 
 const TextBase = styled.span`
@@ -70,10 +72,11 @@ export default function Text({
 Text.defaultProps = {
   tag: 'span',
   variant: 'paragraph1',
+  children: null,
 };
 
 Text.propTypes = {
-  children: PropTypes.node.isRequired,
-  tag: PropTypes.oneOf(['h1', 'h2', 'h3', 'h4', 'h5', 'p', 'li', 'a', 'span']),
+  children: PropTypes.node,
+  tag: PropTypes.oneOf(['h1', 'h2', 'h3', 'h4', 'h5', 'p', 'li', 'a', 'span', 'input']),
   variant: PropTypes.oneOf(['title', 'paragraph1', 'smallestException']),
 };
