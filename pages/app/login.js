c1464cba216078fdbefe35a0fff678f60fffae41
Paulo Henrique Stocco Zancanaro <paulostoc@gmail.com>
2021-03-10 18:43:16 +0000

    feat(login): create login page

---
+++ pages/app/login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function LoginPage() {
+  return (
+    <div>
+      Login
+      <br />
+      <a href="/">
+        Go back to home with refresh
+      </a>
+    </div>
+  );
+}
