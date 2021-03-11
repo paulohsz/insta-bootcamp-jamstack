5d90ebf354532f9c8c86855110450ce072e72675
Paulo Henrique Stocco Zancanaro <paulostoc@gmail.com>
2021-03-11 23:32:12 +0000

    chore: update script test

--- script-test.js
+++ script-test.js
@@ -4,6 +4,8 @@ const shell = require('shelljs');
 
 console.log('Ola mundo');
 
-const resultado = shell.exec('git diff --name-only branch-scripts-testes..main', { silent: true });
+const result = shell.exec('git diff --name-only main...', { silent: true });
+const resultado = shell.exec('git diff --name-only origin/main...', { silent: true });
 
 console.log(resultado.stdout.split('\n'));
+console.log(result.stdout.split('\n'));
